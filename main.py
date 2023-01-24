9516f758caa3e43ae9ab18e612c1b343213c222e
fallnomega <fallnomega@gmail.com>
2023-01-24 11:10:33 -0800

    basic build

--- main.py
+++ main.py
@@ -13,12 +13,32 @@ def divide(a,b):
 operations ={
     "+":"add",
     "-":"subtract",
-    "*":"multiple",
+    "*":"multiply",
     "/":"divide"
 }
 
 print(operations)
 
-num1 = int(input("Whats the first number?: "))
+# num1 = int(input("Whats the first number?: "))
+num1 = 3
+# num2 = int(input("Whats the second number?: "))
+num2 = 10
 
-num2 = int(input("Whats the second number?: "))
+# math = input("What math operation did you want to perform with those two numbers? ( +, - , * , / ) :\n")
+math = '+'
+if math not in operations:
+    print(f"{num1} {math} {num2} is not a valid math problem")
+    exit()
+results=0
+if operations[math]=='add':
+    result=add(num1,num2)
+    print(result)
+elif operations[math]=='subtract':
+    result=subtract(num1,num2)
+    print(result)
+elif operations[math] == 'multiply':
+    result=multiply(num1,num2)
+    print(result)
+else:
+    result=divide(num1,num2)
+    print(result)
