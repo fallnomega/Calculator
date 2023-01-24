d4442af71cf28740f0f629f72a19046e528a73bc
fallnomega <fallnomega@gmail.com>
2023-01-24 12:34:38 -0800

    some recursion

--- main.py
+++ main.py
@@ -8,42 +8,50 @@ def multiply(a,b):
 def divide(a,b):
     return a / b
 
+def do_math_things(a):
+    num3 = int(input("Whats the next number?: "))
+    math2 = input("What  operation did you want to perform? :")
+    new_calculation_function = operations[math2]
+    new_result = new_calculation_function(a,num3)
+    print (new_result)
+    again = input(f"Keep going with previous value {new_result} or exit? y/n :").lower()
+    if again == 'n':
+        print ("Goodbye !")
+        return False
+    else:
+        do_math_things(new_result)
 
 
 operations ={
-    "+":"add",
-    "-":"subtract",
-    "*":"multiply",
-    "/":"divide"
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":divide
 }
 
-print(operations)
+print(logo)
+keep_going=True
+while keep_going == True:
+    additional_math = True
+    num1 = int(input("Whats the first number?: "))
+    num2 = int(input("Whats the second number?: "))
 
-# num1 = int(input("Whats the first number?: "))
-num1 = 3
+    for x in operations:
+        print (x)
+    math = input("What math operation did you want to perform with those two numbers? See above symbol options:")
+    if math not in operations:
+        print(f"{num1} {math} {num2} is not a valid math problem")
+        exit()
+    calculation_function = operations[math]
+    result = calculation_function(num1,num2)
+    print (f"Result so far: {result}")
+    continue_math = input("Continue with this same calculation or exit it? [y/n]").lower()
+    if continue_math=='n':
+        print("Goodbye !")
+        keep_going=False
+    elif continue_math!='n' and continue_math !='y':
+        print("I dont recognize that answer. Goodbye !")
+        keep_going = False
+    else:
+        keep_going = do_math_things(result)
 
-# math = input("What math operation did you want to perform with those two numbers? ( +, - , * , / ) :\n")
-for x in operations:
-    print (x)
-print("What math operation did you want to perform with those two numbers? See above symbol options:")
-math = '*'
-if math not in operations:
-    print(f"{num1} {math} {num2} is not a valid math problem")
-    exit()
-
-# num2 = int(input("Whats the second number?: "))
-num2 = 10
-
-results=0
-if operations[math]=='add':
-    result=add(num1,num2)
-    print(result)
-elif operations[math]=='subtract':
-    result=subtract(num1,num2)
-    print(result)
-elif operations[math] == 'multiply':
-    result=multiply(num1,num2)
-    print(result)
-else:
-    result=divide(num1,num2)
-    print(result)
